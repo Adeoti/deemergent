a2b0af2695cfe76444c9f5129cfeb9172ceb76ea
AdeotiDev Al-Faoz <adeotitempo@gmail.com>
2026-01-23 15:11:16 +0100

    Completed the broadsheet..

--- resources/views/filament/resources/broadsheet-resource/pages/view-broadsheet.blade.php
+++ resources/views/filament/resources/broadsheet-resource/pages/view-broadsheet.blade.php
@@ -203,11 +203,11 @@
                 Print Broadsheet
             </button>
             
-            <button onclick="exportToExcel()"
+            {{-- <button onclick="exportToExcel()"
                 class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 active:bg-blue-600 disabled:opacity-25 transition">
                 <i class="fas fa-file-excel mr-2"></i>
                 Export Excel
-            </button>
+            </button> --}}
             
             <div class="flex-1"></div>
             
